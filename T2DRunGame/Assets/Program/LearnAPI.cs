9e816ea0cf56aac0b13d406c9cc7f851994a742d
Nightrabbit620 <67904646+Nightrabbit620@users.noreply.github.com>
2020-07-29 21:22:30 +0800

    程式語法

---
+++ T2DRunGame/Assets/Program/LearnAPI.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class LearnAPI : MonoBehaviour
+{
+    // 非靜態成員需要
+    // 1. 實體物件或元件 (Unity 階層面板)
+    // 2. 欄位
+    // 先看場景上有的物件與元件
+    public GameObject sphere;
+
+    public Transform tra;
+
+    public Transform cube;
+
+    public void Start()
+    {
+        // ［取得］屬性 Peoperties
+        // ***** 語法：
+        // 欄位名稱.屬性
+        print(sphere.layer);
+
+        print("球的座標：" + tra.position);
+
+        // ［設定］屬性　peoperties
+        // ***** 語法：
+        // 欄位名稱.屬性 = 值
+        tra.localScale = new Vector3(7, 7, 7);
+    }
+
+    public void Update()
+    {
+        // ［使用］方法 Methofs
+        // ***** 語法：
+        // 欄位名稱.方法(對應的引數)
+        cube.Rotate(0f, 0f, 0.5f);
+
+    }
+}
---
+++ T2DRunGame/Assets/Program/LearnAPIStatic.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+
+public class LearnAPIStatic : MonoBehaviour
+{
+    // 認識 API 內有靜態屬性 Static Properties
+    void Start()
+    {
+        // 取得靜態屬性 Static Properties
+        // ***** 語法
+        // 類別.靜態屬性
+        print(Random.value);
+        print(Mathf.PI);
+
+        // ［設定］靜態屬性 Static Properties
+        // ***** 語法
+        // 類別.靜態屬性 = 值
+        // Random.value = 0.999f; // 設定唯獨屬性會有錯誤
+        // 時間進度
+        Time.timeScale = 1f;
+
+        // ［使用］靜態方法 Static Method
+        // ***** 語法
+        // 類別.靜態方法(對應引數)
+        float c = Random.Range(100.9f, 200.5f);
+        print("隨機值：" + c);
+
+        // 整數不包含最大值
+        int d = Random.Range(1, 3);
+        print("隨機整數：" + d);
+
+        // 練習：
+        // 影藏滑鼠 - 指標 Cursor
+        Cursor.visible = false;
+        // -9 取絕對值 - 數學 Mathf
+        print("-9 的絕對值：" + Mathf.Abs(-9));
+    }
+    public void Update()
+    {
+        // print("遊戲時間：" + Time.time)
+
+        // 是否按下任意鍵 - 輸入 Input - true/false
+        print("玩家是否按任意鍵：" + Input.anyKeyDown);
+    }
+}
